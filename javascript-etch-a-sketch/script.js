5d6b690f736684b1da0a2e21b19a187070a4d5dd
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-27 16:36:39 -0300

    Rainbow Backgroundcolor function

--- javascript-etch-a-sketch/script.js
+++ javascript-etch-a-sketch/script.js
@@ -2,13 +2,33 @@ let size = parseInt(document.querySelector("#sizeSlider").value)
 console.log(size)
 let grid = document.querySelector(".grid")
 console.log(grid)
+
 function createDiv() {
   let gridBlock = document.createElement('div')
   gridBlock.classList.add('style-grid-block')
   gridBlock.style.width = `${100.0/size}%`
   gridBlock.style.height = `${100.0/size}%`
+  gridBlock.addEventListener('mouseover', () => {
+    let bgColor = randomBgColor()
+    gridBlock.style.backgroundColor = (bgColor)
+  })
   return gridBlock
 }
+
+function randomBgColor() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor
+}
+
+let clear = document.querySelector(".clear")
+clear.addEventListener("click", () => {
+  document.querySelectorAll(".style-grid-block").forEach(element => {
+    element.style.backgroundColor = "white"
+  })
+})
 
 for (let i = 0; i < (size*size); i++) {
   const block = createDiv()
