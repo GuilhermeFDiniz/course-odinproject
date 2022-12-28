43dfcbf3bb1a1ec1414dbc70593c8752e4a46610
Guilherme Diniz <guidinizeng@gmail.com>
2022-12-28 09:36:02 -0300

    SizeSlider button

--- javascript-etch-a-sketch/script.js
+++ javascript-etch-a-sketch/script.js
@@ -1,43 +1,16 @@
 let size = parseInt(document.querySelector("#sizeSlider").value)
 let grid = document.querySelector(".grid")
 let penColor = "rgb(0,0,0)";
-
-function createDiv() {
-  let gridBlock = document.createElement('div')
-  gridBlock.classList.add('style-grid-block')
-  gridBlock.style.width = `${100.0/size}%`
-  gridBlock.style.height = `${100.0/size}%`
-  gridBlock.addEventListener('mouseover', () => {
-    if (rainbow.value === "true") {
-      gridBlock.style.backgroundColor = (randomBgColor())
-    } else {
-      gridBlock.style.backgroundColor = penColor
-    }
-  })
-  return gridBlock
-}
-
-function randomBgColor() {
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z = Math.floor(Math.random() * 256);
-    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    return bgColor
-}
-
 let rainbow = document.querySelector('.rainbow')
 rainbow.addEventListener('click', () => {
 rainbow.value = true
 })
-
 let picker = document.querySelector("#colorpicker")
 picker.oninput = (e) => {
   rainbow.value = false
   penColor = e.target.value
   console.log(penColor)
 }
-
-
 let clear = document.querySelector(".clear")
 clear.addEventListener("click", () => {
   document.querySelectorAll(".style-grid-block").forEach(element => {
@@ -45,9 +18,46 @@ clear.addEventListener("click", () => {
   })
 })
 
-for (let i = 0; i < (size*size); i++) {
-  const block = createDiv()
-  grid.insertAdjacentElement("beforeend", block)
-  for (let k = 0; k < size; k++) {
+let sizeSlider = document.querySelector("#sizeSlider")
+  sizeSlider.addEventListener('mousemove', (e) => {
+  size = e.target.value
+  document.querySelector(".label-size").textContent = `Select the size: ${size}x${size}`
+  console.log(size)
+  grid.innerHTML = ""
+  createGrid()
+})
+
+function createGrid() {
+
+  for (let i = 0; i < (size*size); i++) {
+    const block = createDiv()
+    grid.insertAdjacentElement("beforeend", block)
+    for (let k = 0; k < size; k++) {
+    }
   }
 }
+
+
+
+  function createDiv() {
+    let gridBlock = document.createElement('div')
+    gridBlock.classList.add('style-grid-block')
+    gridBlock.style.width = `${100.0/size}%`
+    gridBlock.style.height = `${100.0/size}%`
+    gridBlock.addEventListener('mouseover', () => {
+      if (rainbow.value === "true") {
+        gridBlock.style.backgroundColor = (randomBgColor())
+      } else {
+        gridBlock.style.backgroundColor = penColor
+      }
+    })
+    return gridBlock
+  }
+
+  function randomBgColor() {
+      let x = Math.floor(Math.random() * 256);
+      let y = Math.floor(Math.random() * 256);
+      let z = Math.floor(Math.random() * 256);
+      let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+      return bgColor
+  }
