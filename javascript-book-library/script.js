b62e505a8c273791c5347316f1c05b454e0d6ff9
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-06 15:16:41 -0300

    Style mode

---
+++ javascript-book-library/script.js
@@ -0,0 +1,41 @@
+// Screen Mode
+const dark = document.querySelector(".fa-moon")
+dark.addEventListener('click', function (){
+  darkMode()})
+
+const light = document.querySelector(".fa-sun")
+light.addEventListener('click', function (){
+  lightMode()})
+
+function darkMode(){
+  document.body.style.backgroundColor = 'rgb(50, 50, 50)';
+  document.body.style.color = 'white';
+  const screen = document.querySelectorAll(".screen-mode")
+  screen.forEach(element => {
+   element.classList.toggle("show-off-content")
+});
+}
+
+function lightMode() {
+  document.body.style.backgroundColor = 'rgb(255, 225, 175)';
+  document.body.style.color = 'black';
+  const screen = document.querySelectorAll(".screen-mode")
+  screen.forEach(element => {
+   element.classList.toggle("show-off-content")
+});
+}
+
+//
+
+const myLibrary = []
+
+function Book(author, title, pages, read){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary() {
+  myLibrary.push(new Book(author, title, pages, read))
+}
