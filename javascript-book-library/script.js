904f69c6280d7a555093883c1b2158baeff8041d
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-06 17:39:42 -0300

    Form Style

--- javascript-book-library/script.js
+++ javascript-book-library/script.js
@@ -27,6 +27,29 @@ function lightMode() {
 
 //
 
+const addBookModal = document.getElementById('addBookModal')
+const addBookForm = document.getElementById('addBookForm')
+const overlay = document.getElementById('overlay')
+const addBookBtn = document.getElementById('addBookBtn')
+
+const openAddBookModal = () => {
+  addBookForm.reset()
+  addBookModal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+const closeAddBookModal = () => {
+  addBookModal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+const closeAllModals = () => {
+  closeAddBookModal()
+}
+
+addBookBtn.onclick = openAddBookModal
+overlay.onclick = closeAllModals
+
 const myLibrary = []
 
 function Book(author, title, pages, read){
@@ -36,6 +59,7 @@ function Book(author, title, pages, read){
   this.read = read
 }
 
-function addBookToLibrary() {
-  myLibrary.push(new Book(author, title, pages, read))
+function addBookToLibrary(author, title, pages, read) {
+  const book = new Book(author, title, pages, read)
+  myLibrary.push(book)
 }
