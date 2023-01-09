3db494eabac4ae44d768ffee636b5c8bf2b8e514
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-09 06:36:28 -0300

    Fix remove event listener

--- javascript-book-library/script.js
+++ javascript-book-library/script.js
@@ -31,19 +31,98 @@ const addBookForm = document.getElementById('addBookForm')
 const overlay = document.getElementById('overlay')
 const addBookBtn = document.getElementById('addBookBtn')
 const submitBook = document.getElementById('submit-book')
-const author = document.getElementById("author")
-const title = document.getElementById("title")
-const pages = document.getElementById("pages")
-const read = document.getElementById("read")
+const bookContainer = document.querySelector(".container-books")
 const myLibrary = []
 let bookId = 0
 
+
+function createBookCard(book) {
+  const bookCard = document.createElement('div')
+  const bookContent = document.createElement('div')
+  const bookTitle = document.createElement('p')
+  const bookAuthor = document.createElement('p')
+  const bookPages = document.createElement('p')
+  const bookIsRead = document.createElement('p')
+  const buttonGroup = document.createElement('div')
+  const buttonRemove = document.createElement('button')
+  const buttonEdit = document.createElement('button')
+  buttonEdit.textContent = "Edit"
+  buttonRemove.textContent = "Remove"
+  buttonRemove.classList.add('btn-book')
+  buttonEdit.classList.add('btn-book')
+  bookCard.classList.add('book')
+  bookContent.classList.add('book-content')
+  buttonEdit.classList.add('edit')
+  buttonRemove.classList.add('remove')
+  bookContent.appendChild(bookTitle)
+  bookContent.appendChild(bookAuthor)
+  bookContent.appendChild(bookPages)
+  bookContent.appendChild(bookIsRead)
+  buttonGroup.appendChild(buttonRemove)
+  buttonGroup.appendChild(buttonEdit)
+  bookCard.appendChild(bookContent)
+  bookCard.appendChild(buttonGroup)
+  buttonRemove.setAttribute('book-id', book.id)
+  buttonEdit.setAttribute('book-id', book.id)
+  bookCard.setAttribute('book-id', book.id)
+  bookTitle.textContent = `Title: ${book.title}`
+  bookAuthor.textContent = `Author: ${book.author}`
+  bookPages.textContent = `Pages: ${book.pages}`
+  bookIsRead.textContent = `Read: ${book.read}`
+  buttonRemove.addEventListener('click', (event) => {
+    console.log(event.currentTarget.getAttribute('book-id'))
+    myLibrary.forEach((e, index) => {
+      console.log(parseInt(e.id))
+      if(e.id === parseInt(event.currentTarget.getAttribute('book-id'))){
+        myLibrary.splice(index, 1)
+        event.path[2].remove()
+      }
+    })
+  })
+  bookContainer.appendChild(bookCard)
+}
+
+
+
+
+
+
+submitBook.addEventListener('click', (event) => {
+  event.preventDefault()
+  const book = getFromForm()
+  addBookToLibrary(book)
+  createBookCard(book)
+  bookId++
+  addBookModal.classList.remove('active')
+  overlay.classList.remove('active')
+})
+
+function getFromForm() {
+  const author = document.getElementById("author").value
+  const title = document.getElementById("title").value
+  const pages = document.getElementById("pages").value
+  const read = document.getElementById("read").value
+  document.getElementById('author').blur()
+  return new Book(author, title, pages, read, bookId)
+}
+
+function Book(author, title, pages, read = 'false', id){
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.id = id
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
 
 const openAddBookModal = () => {
   addBookForm.reset()
   addBookModal.classList.add('active')
   overlay.classList.add('active')
-  author.focus()
+  document.getElementById('author').focus()
 }
 
 const closeAddBookModal = () => {
@@ -57,70 +136,3 @@ const closeAllModals = () => {
 
 addBookBtn.onclick = openAddBookModal
 overlay.onclick = closeAllModals
-
-function addBookContainer(title,author,pages,read, id) {
-  const bookCard = document.querySelector(".container-books")
-  const bookDiv = document.createElement('div')
-  const bookContent = document.createElement('div')
-  const bookTitle = document.createElement('p')
-  const bookAuthor = document.createElement('p')
-  const bookPages = document.createElement('p')
-  const bookIsRead = document.createElement('p')
-  const buttonGroup = document.createElement('div')
-  const buttonRemove = document.createElement('button')
-  const buttonEdit = document.createElement('button')
-  bookDiv.classList.add('book')
-  bookContent.classList.add('book-content')
-  buttonEdit.classList.add('edit')
-  buttonRemove.classList.add('remove')
-  buttonEdit.textContent = "Edit"
-  buttonRemove.textContent = "Remove"
-  buttonRemove.setAttribute('bookId', id)
-  bookDiv.setAttribute('bookId', id)
-  bookTitle.textContent = `Title: ${title}`
-  bookAuthor.textContent = `Author: ${author}`
-  bookPages.textContent = `Pages: ${pages}`
-  bookIsRead.textContent = `Read: ${read}`
-  bookContent.appendChild(bookTitle)
-  bookContent.appendChild(bookAuthor)
-  bookContent.appendChild(bookPages)
-  bookContent.appendChild(bookIsRead)
-  buttonGroup.appendChild(buttonRemove)
-  buttonGroup.appendChild(buttonEdit)
-  bookDiv.appendChild(bookContent)
-  bookDiv.appendChild(buttonGroup)
-  bookCard.appendChild(bookDiv)
-  buttonRemove.classList.add('btn-book')
-  buttonEdit.classList.add('btn-book')
-  buttonRemove.addEventListener('click', (event) => {
-    myLibrary.forEach((element, index) => {
-      if(toString(element.id) === toString(bookDiv.id)){
-        myLibrary.splice(index, 1)
-      }
-    })
-  })
-}
-
-
-submitBook.addEventListener('click', (event) => {
-  event.preventDefault()
-  addBookToLibrary(author.value, title.value, pages.value, read.value, bookId)
-  addBookContainer(author.value, title.value, pages.value, read.value, bookId)
-  bookId++
-  addBookModal.classList.remove('active')
-  overlay.classList.remove('active')
-})
-
-
-function Book(author, title, pages, read = 'false', id){
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.id = id
-}
-
-function addBookToLibrary(author, title, pages, read, id) {
-  const book = new Book(author, title, pages, read, id)
-  myLibrary.push(book)
-}
