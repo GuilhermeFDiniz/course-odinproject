517ca789115b139ec32424ac93414705410a53d2
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-20 11:02:18 -0300

    Output management

--- basic-webpack-node/src/index.js
+++ basic-webpack-node/src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
-  const element = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-document.body.appendChild(component());
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
