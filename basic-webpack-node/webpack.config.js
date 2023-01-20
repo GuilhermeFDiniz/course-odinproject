24f0a34b62108c9285e1936f033b432f133eb750
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-20 11:47:08 -0300

    Setup for development

--- basic-webpack-node/webpack.config.js
+++ basic-webpack-node/webpack.config.js
@@ -2,19 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
-    teste: './src/teste.js'
+    teste: './src/teste.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
