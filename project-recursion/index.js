fba75495498c16f578c5e44b7e4cbada24349fb7
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-21 16:29:06 -0300

    Fibonnaci done

--- project-recursion/index.js
+++ project-recursion/index.js
@@ -1,10 +1,8 @@
 function fibonacci(number){
   if(number === 1){ return [0] }
   if(number === 2){ return [0,1] }
-   
-  }
 
+  return [...fibonacci(number-1), fibonacci(number-1)[number-2] + fibonacci(number-1)[number-3]]
+}
 
-
-
-console.log(fibonacci(1))
+console.log(fibonacci(5))
