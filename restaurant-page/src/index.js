12c507506ba21d9841c02d13e520ccfc7f7fab5b
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-23 11:32:53 -0300

    Menu.js Done

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,5 @@
-console.log("oi")
+import './assets/style.css';
+import { menu } from "./menu";
+import { home } from "./home";
+import { about } from "./about";
+import _ from 'lodash';
---
+++ restaurant-page/src/menu.js
@@ -0,0 +1,36 @@
+function createMenu(){
+  const menu = document.createElement("div")
+  menu.classList.add('menu')
+  const tomato = createItem('tomato', 'Delicious Tomato Sandwich')
+  menu.appendChild(tomato)
+  const potato = createItem('potato', 'Delicious Potato Sandwich')
+  menu.appendChild(potato)
+  const cheese = createItem('cheese', 'Delicious Cheese Sandwich')
+  menu.appendChild(cheese)
+  return menu
+}
+
+
+function createItem(title, description){
+  const item = document.createElement("div")
+  item.classList.add('menu-item')
+  const menuTitle = document.createElement("h2")
+  menuTitle.textContent = title
+  const menuDescription = document.createElement("p")
+  menuDescription.textContent = description
+  const menuImage = document.createElement("img")
+  menuImage.src = `./images/${title}.png`
+  menuImage.alt = title
+  item.appendChild(menuImage)
+  item.appendChild(menuTitle)
+  item.appendChild(menuDescription)
+  return item
+}
+
+function loadMenu(){
+  const main = document.querySelector('main')
+  main.textContent = ""
+  main.appendChild(createMenu())
+}
+
+export default loadMenu;
