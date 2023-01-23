6cbdfa78dea28eee6ee1f5f4e45fae51254b9055
Guilherme Diniz <guidinizeng@gmail.com>
2023-01-22 23:50:56 -0300

    Mergesort and merge auxiliar func done

--- project-recursion/index.js
+++ project-recursion/index.js
@@ -1,14 +1,12 @@
 function fibonacci(number){
   if(number === 1){ return [0] }
   if(number === 2){ return [0,1] }
-
-  return [...fibonacci(number-1), fibonacci(number-1)[number-2] + fibonacci(number-1)[number-3]]
+  return [...fibonacci(number-1),
+         fibonacci(number-1)[number-2] + fibonacci(number-1)[number-3]]
 }
 
 function mergeSort(list) {
-  if(list.length === 1){
-    return list
-  }
+  if(list.length === 1){ return list }
   let midPoint = list.length/2
   let left = mergeSort(list.slice(0, midPoint))
   let right = mergeSort(list.slice(midPoint))
@@ -16,5 +14,15 @@ function mergeSort(list) {
 }
 
 function merge(leftSide, rightSide){
-
+  const arrayMerged = []
+  while(leftSide.length > 0  && rightSide.length > 0){
+    if(leftSide[0] < rightSide[0]){
+      arrayMerged.push(leftSide.shift())
+    } else {
+      arrayMerged.push(rightSide.shift())
+    }
+  }
+  return arrayMerged.concat(leftSide, rightSide)
 }
+
+console.log(mergeSort([1,3,2,6,4,8,7,200,123]))
